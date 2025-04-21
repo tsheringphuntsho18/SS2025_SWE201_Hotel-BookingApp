4f72e1b4c4d2810f4395e3e1e0f8d1201a306b38
DechenWangdraSherpa <02230281.cst@rub.edu.bt>
2025-04-21 23:48:45 +0600

    changes in UI auth

--- DrukHotel/components/Auth.tsx
+++ DrukHotel/components/Auth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, View, AppState } from "react-native";
+import { Alert, StyleSheet, View, AppState, Text } from "react-native";
 import { supabase } from "../lib/supabase";
 import { Button, Input } from "@rneui/themed";
 
@@ -49,6 +49,7 @@ export default function Auth() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Welcome to HomeStay</Text>
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <Input
           label="Email"
@@ -93,9 +94,16 @@ const styles = StyleSheet.create({
     marginTop: 40,
     padding: 12,
   },
+  title: {
+    marginTop: 100,
+    fontSize: 30,
+    fontWeight: "600",
+    marginBottom: 40,
+    alignSelf: "center",
+  },
   verticallySpaced: {
-    paddingTop: 45,
-    paddingBottom: 42,
+    paddingTop: 4,
+    paddingBottom: 4,
     alignSelf: "stretch",
   },
   mt20: {
